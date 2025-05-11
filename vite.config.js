277f8df91675dbf089222eae528e5fbc1bd91982
antip4uckia <antip4uck.ia@gmail.com>
2025-05-11 13:47:37 +0200

    vercel plug

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()],
   base: process.env.VITE_BASE_PATH || '/movieDB'
   
 })
